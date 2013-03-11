71e8f0a226dc65748343e01ed65781c750c2a7f8
Jos van der Til <jos@vandertil.eu>
2013-03-11 16:41:34 +0100

    Added Build script and CommonAssemblyInfo

---
+++ Build/CommonAssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyCompany("jvandertil")]
+[assembly: AssemblyProduct("NHibernate.BaseInfrastructure")]
+[assembly: AssemblyCopyright("Copyright � Jos van der Til 2012")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyTrademark("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: AssemblyVersion("1.0.0.1")] //Will be replaced
+[assembly: AssemblyFileVersion("1.0.0.1")]
+
+[assembly: NeutralResourcesLanguage("en-US")]
